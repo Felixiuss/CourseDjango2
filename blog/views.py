9befd58cee96716b6dd9eb8053363ab3987f49f2
felix <ramses767@gmail.com>
2020-03-25 16:55:08 +0200

    add template and view for model Category

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,28 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic.base import View
 
-# Create your views here.
+from .models import Category
+
+
+# def home(request):
+#     if request.method == "POST":
+#         return HttpResponse('Hi')
+#     else:
+#         return HttpResponse('Good')
+
+
+class HomeView(View):
+    """Home page"""
+    def get(self, request):
+        category_list = Category.objects.all()
+        # print(category_list)
+        return render(request, 'blog/home.html', {'categories': category_list})
+
+
+class CategoryView(View):
+    """Output of category articles"""
+    def get(self, request, category_name):
+        category = Category.objects.get(slug=category_name)
+        return render(request, 'blog/post_list.html', {'category': category})
+
